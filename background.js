5559a9b8d47bc5949c6d952a9b678a7060a53ce7
Shubhadeep Das <shubhadeep.das2016@vitstudent.ac.in>
2019-05-05 20:32:07 +0530

    Added popup

---
+++ background.js
@@ -0,0 +1,9 @@
+console.log("Background script running");
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // if (changeInfo.status === "complete") {
+  const message = { status: "complete" };
+  chrome.tabs.sendMessage(tabId, message);
+  console.log("Sent message");
+  // }
+});
--- content.js
+++ content.js
@@ -1,44 +1,44 @@
 console.log("Github extension running");
 
-const HALLOWEEN_GRAPH = {
-  "#ebedf0": "#ebedf0",
-  "#c6e48b": "#fdf436",
-  "#7bc96f": "#ffc700",
-  "#239a3b": "#ff9100",
-  "#196127": "#06001c"
+const DEFAULT_GRAPH = ["#ebedf0", "#c6e48b", "#7bc96f", "#239a3b", "#196127"];
+
+const HALLOWEEN_GRAPH = ["#ebedf0", "#fdf436", "#ffc700", "#ff9100", "#06001c"];
+
+const BLUE_GRAPH = ["#ebedf0", "#c0ddf9", "#73b3f3", "#3886e1", "#17459e"];
+
+const getAllDaysRect = columns => {
+  let dayRects = [];
+
+  columns.forEach(column => {
+    dayRects = [...dayRects, ...column.children];
+  });
+
+  return dayRects;
 };
 
-const BLUE_GRAPH = {
-  "#ebedf0": "#ebedf0",
-  "#c6e48b": "#c0ddf9",
-  "#7bc96f": "#73b3f3",
-  "#239a3b": "#3886e1",
-  "#196127": "#17459e"
-};
-
-const updateGraph = _ => {
+const updateGraph = chosen => {
   const graph = document.querySelector(".js-calendar-graph-svg > g");
 
   if (graph !== null) {
     const legend = document.querySelectorAll(".legend li");
     const columns = [...graph.children].filter(child => child.nodeName === "g");
 
-    columns.forEach(column => {
-      const days = [...column.children];
+    const days = getAllDaysRect(columns);
 
-      days.forEach(day => {
-        const newColor = HALLOWEEN_GRAPH[day.getAttribute("fill")];
+    days.forEach(day => {
+      const idx = DEFAULT_GRAPH.indexOf(day.getAttribute("fill"));
+      const newColor = chosen[idx];
 
-        day.setAttribute("fill", newColor);
-      });
+      day.setAttribute("fill", newColor);
     });
 
-    const colors = Object.values(HALLOWEEN_GRAPH);
-
     legend.forEach((li, idx) => {
-      li.style.backgroundColor = colors[idx];
+      li.style.backgroundColor = chosen[idx];
     });
   }
 };
 
-updateGraph();
+/**
+ * Initial load
+ */
+updateGraph(HALLOWEEN_GRAPH);
