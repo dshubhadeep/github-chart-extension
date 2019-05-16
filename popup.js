ff8a215a1c86b04220a794cbb8c9a8af83c0057c
Shubhadeep Das <shubhadeep.das2016@vitstudent.ac.in>
2019-05-16 12:07:25 +0530

    Added popup logic

--- popup.js
+++ popup.js
@@ -1,14 +1,42 @@
 console.log("Popup");
 
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("loaded");
-  const header = document.getElementById("header");
-  console.log(header);
-  header.addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const tab = tabs[0];
-      console.log(tab);
-      chrome.tabs.sendMessage(tab.id, { text: "Clicked" });
+document.addEventListener("DOMContentLoaded", () => {
+  const themeWrappers = document.querySelectorAll(".theme-wrapper");
+
+  /**
+   * Initial load
+   */
+
+  let savedTheme;
+
+  chrome.storage.local.get("theme", result => {
+    console.log(`Saved theme : ${result.theme}`);
+    savedTheme = result.theme;
+
+    // Add selected class to theme-wrapper
+    updateWrappers(savedTheme);
+  });
+
+  themeWrappers.forEach(themeWrapper => {
+    themeWrapper.addEventListener("click", _ => {
+      const theme = themeWrapper.className.split(" ")[1];
+
+      updateWrappers(theme);
+
+      // Add theme to localStorage
+      chrome.storage.local.set({ theme }, () => {
+        console.log(`Saved theme : ${theme}`);
+      });
     });
   });
+
+  const updateWrappers = theme => {
+    themeWrappers.forEach(themeWrapper => {
+      if (themeWrapper.classList.contains(theme)) {
+        themeWrapper.classList.add("selected");
+      } else {
+        themeWrapper.classList.remove("selected");
+      }
+    });
+  };
 });
